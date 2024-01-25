aad6ae29a3ef21b9e62304f4eaa4f101d6afb10c
artemlarchenkov <artem.bond.2000@mail.ru>
2024-01-25 17:00:21 +0300

    Добавление функции скользящего среднего:

--- moving_average/moving_average.cpp
+++ moving_average/moving_average.cpp
@@ -1,5 +1,26 @@
 ﻿#include <iostream>
+#include <vector>
 
+
+template <typename T>
+std::vector<T> simpleMovingAverage(const std::vector<T>& data, int windowSize) {
+    std::vector<T> result;
+    double sum = 0.0;
+
+    // Скользящее среднее для начального окна
+    for (int i = 0; i < windowSize; ++i) {
+        sum += data[i];
+        result.push_back(sum / (windowSize));
+    }
+
+    //  Скользящее среднее для остальных данных
+    for (int i = windowSize; i < data.size(); ++i) {
+        sum = sum + data[i] - data[i - windowSize];
+        result.push_back(sum / windowSize);
+    }
+
+    return result;
+}
 int main() {
 
 
